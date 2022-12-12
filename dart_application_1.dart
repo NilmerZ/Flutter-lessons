f1898d506b38271024140ecd07ca4ae32369de96
NilmerZ <nilmerzilmer@gmail.com>
2022-12-12 09:50:06 +0300

    Update dart_application_1.dart

--- dart_application_1.dart
+++ dart_application_1.dart
@@ -1,92 +1,120 @@
+import 'dart:math';
 
-void main () {
+void main() {
+  // If else практика - 1.
+    var intValue = Random().nextInt(6);
+    print(intValue);
+    if (intValue == 1) {
+      print('Большой палец');
+    } else if (intValue == 2) {
+      print('Указательный палец');
+    } else if (intValue == 3) {
+      print('Средний палец');
+    } else if (intValue == 4) {
+      print('Безымянный палец');
+    } else if (intValue == 5) {
+      print('Мезинец');
+    } else if (intValue == 6) {
+      print('Error');
+  }
 
-// Задание 1
- var List1  = [1, 7, 12, 3, 56, 2, 87, 34, 54];
- var list =  [1, 7, 12, 3, 56, 2, 87, 34, 54];
+  // If else практика - 2.
 
-print(List1.first);
-print(List1[4]);
-print(List1.last);
- print(list.first);
- print(list[4]);
-print(list.last);
+  var min = Random().nextInt(59);
 
-// Задание 2
-var list2 = [3, 12, 43, 1, 25, 6, 5, 7];
-var list3 = [55, 11, 23, 56, 78, 1, 9];
+  print(min);
+  if (min <= 15) {
 
-print(list2 + list3);
+  print('1-четверть часа');
+  }else if (min == 16 <= 30){
+    print ('2-четверть часа');
+  }else if (min == 31 <= 44){
+    print('3-четверть часа');
+  }else if (min == 45 <= 59){
+    print('4-четверть часа');
+  }  
 
-// Задание 3
-// Решение 1 вариант
-//var list4  = ['a','d','F','l','u','t','t','e','R','y','3','b','h','j'];
-//var list5 = ['F','l','u','t','t','e','R'];
-var listOne = [3, 12, 43, 1, 25, 6, 5, 7];
-var listTwo = [55, 11, 23, 56, 78, 1, 9];
+  // If else практика - 3.
+  String lang = 'Eng';
+  List arr=[];
+  List q = ['Пн', 'Вт','Ср','Чт','Пт','Сб','Вс'];
+  List w = ['Su','Mo','Tu','We','Th','Fr','Sa'];
 
-listOne.addAll(listTwo);
-print (listOne);
+  if (lang == 'Ru') {
+  arr = q;
+  } else if (lang == 'Eng') {
+    arr = w;
+  } else
+    print('Error');
+  print(arr);
 
-var set1= ['a','d','F','l','u','t','t','e','R','y','3','b','h','j'];
+  // If else практика - 3.1
+  
+String a = 'abcde';
+  if (a[0] == 'a') {
+    print('Yes');
+  } else {
+    print('No');
+  }
 
-var set2 =['F','l','u','t','t','e','R'];
+ // If else практика - 3.2
 
-//задание 3 Решение 2 вариант 
-final characters1 = <String>{'a','d','F','l','u','t','t','e','R','y','3','b','h','j'};
-final characters2 = <String>{'F','l','u','t','t','e','R'};
-final unionSet = characters1.intersection(characters2);
-print(unionSet);
+   var num = Random().nextInt(5);
+  
+  String result;
 
+     if (num == 1) {
+      result = 'Зима';
+    } else if (num == 2) {
+      result = 'Весна';
+    } else if (num == 3) {
+      result = 'Лето';
+    } else if (num == 4) {
+      result = 'Осень';
+    } else {
+      result = 'Ошибка';
+    
+    }
+print(result);
+print(num);
 
-// Задание 4
-var listone = [1, 2, 3, 4, 5, 6, 7];
-
-int val = 3;
-
-print(listone.contains(val));
-print(listone.first);
-print(listone.last);
-print(listone.length);
-
-
-// Задание 5
-var list5 =  [601, 123, 2, "dart", 45, 95, "dart24", 1];
-
-String a = 'dart';
-String b = '951';
-
-print(a.contains(a));
-print(a.contains(b));
-
-// Задание 6
-var list6 =  ['post', 1, 0, 'flutter'];
-String myDart = 'Flutter';
-
-print(list6.contains('Flutter'));
-
-
-
-
-// Задание 7 
-
-var listtwo = ['I', 'Started', 'Learn', 'Flutter', 'Since', 'April'];
-//var listtwo = ['"I", "Started", "Learn", "Flutter", "Since", "April"'];
-
-var myFlutter =  listtwo.join('*');
-
-
-print(myFlutter);
-
-// Задание 8 
-
-final numbers = <int> [1, 9, 3, 195, 202, 2, 5, 7, 9, 10, 3, 15, 0, 11];
-
-numbers.sort();
-print(numbers);
-
+// If else практика - 3.3
+int xxx = -3;
+if (xxx <= 0){
+  print('True');
+} else print('false');
 
 
+// If else практика - 4
+String v = '123123';
+
+int sumone  = int.parse(v[0])+int.parse(v[1])+int.parse(v[2]);
+int sumtwo  = int.parse(v[3])+int.parse(v[4])+int.parse(v[5]);
+
+if (sumone == sumtwo){
+  print('True');
+}else print('false');
+
+
+// If else практика - 5
+
+
+ String signal = 'green';
+ String ary = '';
+  String qw = 'стоять';
+  String we = 'приготовиться';
+  String er = 'идти';
+
+
+  if (signal == 'yelow') {
+  ary = we;
+  } else if (signal == 'green') {
+    ary = er;
+  } else if (signal == 'red') {
+    ary = qw;
+  }
+  print(ary);
 
 
 }
+
